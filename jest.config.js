8cf0f08f407d570eb7a7a925a54ddae3645271a1
sanding0 <roller.yxr@gmail.com>
2023-02-20 17:45:47 +0800

    test: cache & debounce

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
+/** @type { import('jest').Config} */
 module.exports = {
   verbose: true,
   roots:['<rootDir>/tests/'],
-  testRegex:['.*\\.(spec|test)\\.[jt]sx?$']
+  testRegex:['.*\\.(spec|test)\\.[jt]sx?$'],
+  testTimeout:30000
 }
